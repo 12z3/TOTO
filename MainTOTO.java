45d07946a0eb9adc798ee0e28f67c3f13a0aad23
12z3 <77529497+12z3@users.noreply.github.com>
2022-12-25 07:23:36 +0200

    Add files via upload

--- MainTOTO.java
+++ MainTOTO.java
@@ -1,11 +1,17 @@
 package task.TOTO.Projects;
 
-import java.io.IOException;
+import training.Methods;
 
-public class MainTOTO {
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+public class MainTOTO extends Methods {
     public static void main(String[] args) throws IOException {
 
+        long start = getStartTime();
         TOTO toto = new TOTO();
         toto.play();
+        long end = getEndTime(start);
+        System.out.println("Compile Time: " + (end - start) / 6000 + " s");
     }
 }
