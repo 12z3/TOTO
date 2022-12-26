49f73d411e7d32449dbd791fa7c9004bc7d9d46e
12z3 <77529497+12z3@users.noreply.github.com>
2022-12-26 20:57:06 +0200

    Add files via upload

--- RegEx.java
+++ RegEx.java
@@ -1,5 +1,8 @@
 package task;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegEx {
     /**
      * <a href="https://regexr.com/">...</a>
@@ -10,7 +13,7 @@ public class RegEx {
         ///String a = "slkdj, 12, 34 *&^ H";
         String aa = " 12";
 
-        String b = " 3, 23, 45, 33, 5, 44";
+        String b = " 8, 16, 27, 29, 38, 12";
         String b4 = "3, 3, 5, 3, 5, 4";
         String b2 = "-13, 23, 45, 33, 5, 44";
         String b3 = "asd, 23, 45, 33, 5, 44";
@@ -29,6 +32,7 @@ public class RegEx {
         String regex1 = "[0-9]+";
 
         System.out.println(isValidInputList(b));
+        System.out.println(isValidInputList1(b));
 
 //        boolean isNumeric = a.chars().allMatch(Character::isDigit);
 //        System.out.println(isNumeric);
@@ -36,14 +40,30 @@ public class RegEx {
     }
 
     private static boolean isValidInputList(String input) {
+        System.out.println(input);
         int countDigit = 0;
-        String regex = "\\d{2}?|\\d";                                // ... гарантира, че е число 23, 5
+        //String regex = "\\d{1,2}";                                // ... гарантира, че е число 23, 5
+        //String regex = "\\d{2} ?|\\d";
+        //String regex = "^(0?[1-9]|[1-4][0-9])(,(0?[1-9]|[1-4][0-9]))*$";
+        String regex = "^(0?[1-9]|[1-4][0-9])(,(0?[1-9]|[1-4][0-9]))*$";
+        //String regex = "^\\d{1,2}(,\\d{1,2})*$";
         String[] tmp = input.trim().split(", ");
 
         for (String s : tmp) {
             if (s.matches(regex)) countDigit++;
         }
         return countDigit == tmp.length;
+    }
+
+    private static boolean isValidInputList1(String input){
+        System.out.println(input);
+        Pattern pattern = Pattern.compile("^(0?[1-9]|[1-4][0-9])(,(0?[1-9]|[1-4][0-9]))*$");
+        //Pattern pattern = Pattern.compile("^(0?[1-9]|[1-4][0-9])(,(0?[1-9]|[1-4][0-9]))*$");
+        //Pattern pattern = Pattern.compile("\\d{2} ?|\\d");
+        //Pattern pattern = Pattern.compile("\\d{1,2}");
+        Matcher matcher = pattern.matcher(input);
+
+        return matcher.matches();
     }
 
 
