53ba688140ee0449d690fb3ac0ea60d0402df987
12z3 <77529497+12z3@users.noreply.github.com>
2023-01-06 19:57:00 +0200

    Add files via upload

--- TimeToTOTO.java
+++ TimeToTOTO.java
@@ -1,4 +1,4 @@
-package task.TOTO.TotoTime;
+package task.TOTOOLD.TotoTime;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -12,13 +12,16 @@ public class TimeToTOTO {
     }
 
     private static LocalDateTime getLocalDateTime() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Кога е следващият Тираж? " +
-                "\nВъведи (година месец ден час минути) разделени с интервал: ");       // next:2022 12 25 18 45
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Кога е следващият Тираж? " +
+//                "\nВъведи (година месец ден час минути) разделени с интервал: ");       // next:2023 01 05 18 45
 
-        String[] dataTimeFormatAnswer = scanner.nextLine()
+        String currentDateTime = "2023 01 08 18 45";
+
+        String[] dataTimeFormatAnswer = currentDateTime
                 .trim()
                 .split(" ");
+
         int year = Integer.parseInt(dataTimeFormatAnswer[0]);
         int month = Integer.parseInt(dataTimeFormatAnswer[1]);
         int dayOfMonth = Integer.parseInt(dataTimeFormatAnswer[2]);
@@ -29,14 +32,25 @@ public class TimeToTOTO {
     }
 
     private static void whatTimeToTotoIs(LocalDateTime timeOfToto) {
-        DateTimeFormatter formatDate = DateTimeFormatter.ofPattern("dd MMM yyyy, E - a c 'ден:' HH:hh:ss ч ");
+        DateTimeFormatter formatDate = DateTimeFormatter.ofPattern("dd MMM yyyy, E - a- c 'ден:' HH:mm:ss ч ");
+        DateTimeFormatter formatDateA = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
 
         LocalDateTime now = LocalDateTime.now();
 
-        int count = 0;
+        int dYear = timeOfToto.getYear() - now.getYear();
         int dDays = timeOfToto.getDayOfMonth() - now.getDayOfMonth();
+        int dMinutes = timeOfToto.getMinute() - now.getMinute();
+        int count = 0;
 
-       while (dDays < 0) {
+        if (dYear == 0) {
+            dDays = timeOfToto.getDayOfMonth() - now.getDayOfMonth();
+        } else if (dYear < 0 || dYear > 2) {
+            dDays = -1;
+        } else {
+            dDays = 31 - (now.getDayOfMonth() + timeOfToto.getDayOfMonth());
+        }
+
+        while (dDays < 0) {
             System.out.println("... Я си оправи времената");
             dDays = getLocalDateTime().getDayOfMonth() - now.getDayOfMonth();
         }
@@ -44,17 +58,28 @@ public class TimeToTOTO {
             dDays--;
             count++;
         }
+
         long hour1 = now.getHour();
         long hour2 = timeOfToto.getHour();
-        long dHours = Math.abs(hour2 - hour1);
+        long dHours = (hour2 - hour1);
+        if (dHours < 0) {
+            dHours = 24 - (hour1 - hour2);
+            //dDays--;
+            count--;
+        }
 
         long min1 = now.getMinute();
         long min2 = timeOfToto.getMinute();
-        long dMins = Math.abs(min2 - min1);
+        long dMins = (min2 - min1);
+        if (dMins < 0) {
+            dMins = 60 - (min1 - min2);
+            dHours--;
+        }
 
-       // if (dDays == 1 && dHours == 0 && dMins == 0) dDays = 0;
 
-            System.out.println("The Day is: " + timeOfToto.format(formatDate));
+        // if (dDays == 1 && dHours == 0 && dMins == 0) dDays = 0;
+
+        System.out.println("The Day is: " + timeOfToto.format(formatDate));
         System.out.println("ToDay is: " + now.format(formatDate) + "\n" + "\n"
                 + "Reminders: "
                 + count + " days (in " + timeOfToto.getDayOfWeek() + ") - "
