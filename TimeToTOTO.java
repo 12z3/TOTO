c0c53583de77a26717c311404e3383c691fec74e
12z3 <77529497+12z3@users.noreply.github.com>
2023-01-30 17:40:23 +0200

    Add files via upload

--- TimeToTOTO.java
+++ TimeToTOTO.java
@@ -10,9 +10,10 @@ public class TimeToTOTO {
         whatTimeToTotoIs(ldt);
     }
 
+    // todo: При текуща дата "2023 01 30 18 45" и следваща "2023 02 02 18 45" резултата е Идиотски.
     private static LocalDateTime getLocalDateTime() {
 
-        String currentDateTime = "2023 01 29 18 45 ";
+        String currentDateTime = "2023 02 02 18 45 ";
         String[] dataTimeFormatAnswer = currentDateTime
                 .trim()
                 .split(" ");
@@ -33,45 +34,47 @@ public class TimeToTOTO {
         LocalDateTime now = LocalDateTime.now();
 
         int dYear = timeOfToto.getYear() - now.getYear();
-        int dDays = timeOfToto.getDayOfMonth() - now.getDayOfMonth();
+        int dDays = timeOfToto.getDayOfYear() - now.getDayOfYear();
         long hour1 = now.getHour();
         long hour2 = timeOfToto.getHour();
         long dHours = (hour2 - hour1);
-        int count = 0;
+        long min1 = now.getMinute();
+        long min2 = timeOfToto.getMinute();
+        long dMins = (min2 - min1);
+        int count = 0, count1 = 0, count2 = 0;
 
         //TODO: Как ще направиш проверката за минала дата?
 
         if (dYear == 0) {
-            dDays = timeOfToto.getDayOfMonth() - now.getDayOfMonth();
+            dDays = timeOfToto.getDayOfYear() - now.getDayOfYear();
         } else if (dYear < 0 || dYear > 2) {
             dDays = -1;
         } else {
-            dDays = 31 - (now.getDayOfMonth() + timeOfToto.getDayOfMonth());
+            dDays = 31 - (now.getDayOfYear() + timeOfToto.getDayOfYear());
         }
 
-        if (dHours < 0) {
-            dHours = 24 - (hour1 - hour2);
-            count--;
-        }
+//        if (timeOfToto.getMonth() == now.getMonth()) {
+            if (dHours < 0) {
+                dHours = 24 - (hour1 - hour2);
+                count--;
+            }
+            if (dMins < 0) {
+                dMins = 59 - (min1 - min2);
+                dHours--;
+            }
 
-        long min1 = now.getMinute();
-        long min2 = timeOfToto.getMinute();
-        long dMins = (min2 - min1);
-        if (dMins < 0) {
-            dMins = 59 - (min1 - min2);
-            dHours--;
-        }
+            while (dDays != 0) {
+                dDays--;
+                count++;
+            }
 
-        while (dDays != 0) {
-            dDays--;
-            count++;
-        }
-
-        if (count < 0) {
-            System.out.print("Има нещо сбъркано в ДАТАТА");
-            return;
-        }
-
+            if (count < 0) {
+                System.out.print("Има нещо сбъркано в ДАТАТА");
+                return;
+            }
+//        } else {
+//           //todo: ? (текущият ден + 7) > 30(31)
+//        }
 
         System.out.println("The Day is: " + timeOfToto.format(formatDate));
         System.out.println("ToDay is: " + now.format(formatDate) + "\n" + "\n"
