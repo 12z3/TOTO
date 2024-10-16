d5594acda6ee7e59bb1a8d3912663c933aa4ac14
12z3 <77529497+12z3@users.noreply.github.com>
2024-10-17 01:02:44 +0300

    Add files via upload

--- TotoWithSet.java
+++ TotoWithSet.java
@@ -1,28 +1,61 @@
 package Jackpot;
 
+import java.io.File;
+import java.io.FileWriter;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class TotoWithSet {
-	public static void main(String[] args) {
-		play();
+
+	// todo: Използвай филтър на Калман за да предсакжеш следващото най-вероятно std и на базата на него си
+	//       генерирай масива ;) (Разпределението на std във времето го имаш.)
+	//       С LabView си генерирай файла и дерзай.
+
+	// todo: Помисли за оптимизация на търсенето, най-малкото bs ти е под ръка. Че е меко казано грозно в този си вид.
+
+	private final int matchedSums = 9;
+	private final double std = 11;
+
+	public static void main(String[] args) throws IOException {
+		new TotoWithSet().play();
 	}
 
-	private static void play() {
+	//	Generates sets of lottery numbers with a target standard deviation and number of matches to a previous draw.
+	private void play() throws IOException {
 //		List<List<Integer>> suppose = digitsGenerator(3);
 //		printRes(suppose);
-		List<Integer> officialRs = new ArrayList<>(List.of(5, 19, 21, 22, 47, 48));
-		double officialRsStd = stdOfRow(officialRs);
+		List<Integer> officialRes = new ArrayList<>(List.of(13, 17, 23, 30, 34, 48));
+		double officialRsStd = stdOfRow(officialRes);
+		System.out.println(officialRes);
 
 //		for (List<Integer> row : suppose) {
 //			System.out.print(checkMatchesByRow(row, officialRs) + "   ");
 //		}
 //		System.out.println();
-		long  start = System.nanoTime();
-		List<List<Integer>> res = generatorByMatches(officialRs, 8, 12.5);
+		long start = System.nanoTime();
+		List<List<Integer>> res = generatorByMatches(officialRes, this.matchedSums, this.std);
 		printRes(res);
 		TestStd(res);
 		double duration = (System.nanoTime() - start) / Math.pow(10, 9);
-		System.out.printf("\nduration: %.2f s", duration);
+
+		if (duration >= 60 && duration < 3600) {
+			System.out.printf("\nduration: %.0f min", Math.ceil(duration) / 60);
+		} else if (duration >= 3600) {
+			System.out.printf("\nduration: %.2f hour", Math.ceil(duration) / 3600);
+		} else if (duration < 60) {
+			System.out.printf("\nduration: %.0f s", Math.ceil(duration));
+		} else {
+			System.out.println("\n404: Нещо безвъзвратно се е объркало ;)");
+		}
+
+		System.out.println();
+		int[][] tmpResMatrix = print2DRes(res, officialRes);
+		writer(res, officialRes, duration, this.matchedSums, std);
 	}
 
 	private static void printRes(List<List<Integer>> suppose) {
@@ -36,7 +69,7 @@ public class TotoWithSet {
 
 	private static List<List<Integer>> digitsGenerator(int cntOfRows) {
 		List<List<Integer>> suppose = new ArrayList<>();
-		Random rnd = new Random();
+		SecureRandom rnd = new SecureRandom();
 		Set<Integer> digits;
 		int cnt = 0;
 
@@ -54,7 +87,7 @@ public class TotoWithSet {
 	}
 
 	private static List<Integer> sampleGenerator(int numOfDigits) {
-		Random random = new Random();
+		SecureRandom random = new SecureRandom();
 		Set<Integer> digits = new HashSet<>();
 
 		while (digits.size() < numOfDigits) {
@@ -67,7 +100,7 @@ public class TotoWithSet {
 	}
 
 
-	// Генерира три масива чиито илементи имат средноквадратично отклонение = std
+	// Генерира три масива чиито елементи имат средноквадратично отклонение = std
 	private static List<List<Integer>> generatorByStd(double std) {
 		int cnt = 0;
 		double tmpStd = Integer.MIN_VALUE;
@@ -95,7 +128,8 @@ public class TotoWithSet {
 														  int matchesSum, double std) {
 		List<List<Integer>> suppose = null;
 		int tmpSum = 0, attempt = 0;
-		while (tmpSum <= matchesSum) {
+
+		while (tmpSum < matchesSum) {
 			tmpSum = 0;
 			suppose = generatorByStd(std);
 			for (List<Integer> row : suppose) {
@@ -103,7 +137,6 @@ public class TotoWithSet {
 			}
 			attempt++;
 		}
-		System.out.println();
 		System.out.println("Attempt:  " + attempt + " <- ");
 		return suppose;
 	}
@@ -142,5 +175,140 @@ public class TotoWithSet {
 				System.out.print(stdOfRow(tmp.get(i)) + " | ");
 			} else System.out.print(stdOfRow(tmp.get(i)));
 		}
+	}
+
+	static Double setTime(double time) {
+		int hour = (int) time;
+		int minutes = (int) (time - hour) * 60;
+		String hStr, mStr, resStr;
+		hStr = String.valueOf(hour);
+		mStr = String.valueOf(minutes);
+		resStr = hStr + mStr;
+
+		return Double.parseDouble(resStr);
+	}
+
+	private static int[][] print2DRes(List<List<Integer>> res, List<Integer> officialRes) {
+		int[][] matrix = fill649Arr();
+		int[][] tmp = new int[0][];
+
+		for (int i = 0; i < res.size(); i++) {
+			List<Integer> thisRow = res.get(i);
+			tmp = searchIntoThisRow(thisRow, matrix);
+		}
+		return tmp;
+	}
+
+	private static int[][] searchIntoThisRow(List<Integer> thisRow, int[][] matrix) {
+		int[][] tmp = new int[7][7];
+		for (int el : thisRow) {
+			for (int i = 0; i < matrix.length; i++) {
+				int idx = bs(matrix[i], el);
+				if (idx != -1) {
+					tmp[i][idx] = el;
+				}
+			}
+		}
+		print2D(tmp);
+		return tmp;
+	}
+
+	private static void print2D(int[][] tmp) {
+		System.out.println();
+		for (int[] arr : tmp) {
+			for (Integer el : arr) {
+				if (el != 0) {
+					System.out.print(el + " ");
+				} else System.out.print(" - ");
+			}
+			System.out.println();
+		}
+	}
+
+	private static int[][] fill649Arr() {
+		int[][] arr = new int[7][7];
+		int cnt = 1;
+		for (int i = 0; i < 7; i++) {
+			for (int j = 0; j < 7; j++) {
+				arr[i][j] = cnt;
+				cnt++;
+			}
+		}
+		return arr;
+	}
+
+	static void writer(List<List<Integer>> res,
+					   List<Integer> officialResult, double duration, double matchedSum, double std) throws IOException {
+		LocalDateTime time = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss dd-MM-yyyy");
+		String currentTime = time.format(formatter);
+		File file = new File(
+				 currentTime + ".txt");
+		FileWriter writer = new FileWriter(file, StandardCharsets.UTF_8);
+
+		writer.write(currentTime + "\n");
+		writer.write("\n" + "Official result: " + officialResult + "\n");
+		writer.write("Matched Sum: " + matchedSum + " - " + "Std: " + std + "\n");
+
+		if (duration >= 60 && duration < 3600) {
+			String timeX = String.valueOf(Math.ceil((duration) / 60)).formatted("%.0f");
+			writer.write(timeX + " min" + "\n");
+		} else if (duration >= 3600) {
+			String timeX = String.valueOf(Math.ceil((duration) / 3600)).formatted("%.0f");
+			writer.write(timeX + " hour" + "\n");
+		} else if (duration < 60) {
+			writer.write(Math.ceil(duration) + " sec" + "\n");
+		} else {
+			System.out.println("\n404: Нещо безвъзвратно се е объркало ;)\n");
+		}
+		writer.write("\n");
+
+
+		for (List<Integer> row : res) {
+			for (Integer el : row) {
+				writer.write(el + " ");
+			}
+			writer.write("\n");
+		}
+		writer.write("\n");
+
+		int[][] matrix = fill649Arr();
+		for (List<Integer> row : res) {
+			int[][] tmp = new int[7][7];
+			for (int el : row) {
+				for (int i = 0; i < matrix.length; i++) {
+					int idx = bs(matrix[i], el);
+					if (idx != -1) {
+						tmp[i][idx] = el;
+					}
+				}
+			}
+
+			for (int i = 0; i < tmp.length; i++) {
+				for (int j = 0; j < tmp[i].length; j++) {
+					if (tmp[i][j] != 0) {
+						writer.write(String.valueOf(tmp[i][j] + " "));
+					} else writer.write(" - ");
+				}
+				writer.write("\n");
+			}
+			writer.write("\n");
+		}
+		writer.close();
+	}
+
+	static int bs(int[] arr, int target) {
+		int s = 0, e = arr.length - 1, m;
+
+		while (s <= e) {
+			m = s + (e - s) / 2;
+
+			if (arr[m] == target) {
+				return m;
+			} else if (arr[m] < target) {
+				s = m + 1;
+			} else e = m - 1;
+		}
+		return -1;
 	}
 }
